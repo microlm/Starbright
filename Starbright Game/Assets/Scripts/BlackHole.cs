b9ddcce1b7dd7a71972b1bcf7881fcd9d150f9ec
ggetz <ggetz881@gmail.com>
2014-11-24 23:11:58 -0500

    New build and stripped down black holes

---
+++ Starbright Game/Assets/Scripts/BlackHole.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackHole : MonoBehaviour {
+	
+	public float gravityDampening;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerCharacter.instance.BodyComponent.Gravitiate (this, gravityDampening);
+	}
+
+	public Vector3 Position {
+		get { return gameObject.transform.position; }
+	}
+}
