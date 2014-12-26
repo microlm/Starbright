8a9931fd63c193c01b45b095ff98124d8e23e404
ggetz <ggetz881@gmail.com>
2014-12-26 13:18:19 -0500

    Black Holes Grow
    Black Holes grow in sclae by Growth Speed

--- Starbright Game/Assets/Scripts/BlackHole.cs
+++ Starbright Game/Assets/Scripts/BlackHole.cs
@@ -3,19 +3,30 @@ using System.Collections;
 
 public class BlackHole : MonoBehaviour {
 	
-	public float gravityDampening = 1f;
-
-	// Use this for initialization
+	public float GravityDampening = 1f;
+	public float GrowthSpeed = 0.0005f;
+	
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		PlayerCharacter.instance.BodyComponent.Gravitiate (this, gravityDampening);
+		PlayerCharacter.instance.BodyComponent.Gravitiate (this, GravityDampening);
+		Scale += Scale * GrowthSpeed;
+	}
+
+	void OnCollisionEnter2D(Collision2D c)
+	{
+		
 	}
 
 	public Vector3 Position {
 		get { return gameObject.transform.position; }
+		set { gameObject.transform.position = value; }
+	}
+
+	public Vector3 Scale {
+		get { return gameObject.transform.localScale; }
+		set { gameObject.transform.localScale = value; }
 	}
 }
