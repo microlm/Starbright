a7964eb28fb8d128cf41b69daaa4e389db06ea44
ggetz <ggetz881@gmail.com>
2014-12-28 22:44:09 -0500

    High Scores & saving 'em
    New HighScore class. Not implemented anywhere yet, but pretty self
    explanatory when we want to use it, just check comments. Make a
    HighScore Object, use .Scores to retreive scores, .AddScore(score) to
    add a score, and .ClearScores() if you wanna reset.

---
+++ Starbright Game/Assets/Scripts/HighScores.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScores {
+
+	private int[] scores;
+	private int numScores = 5;
+
+	public HighScores() 
+	{
+		scores = new int[numScores];
+		LoadScores ();
+	}
+
+	/** Array of player scores from high to low */
+	public int[] Scores
+	{
+		get { return scores; }
+	}
+
+	/** Adds score to list of high scores and saves it to memory, will cause a slow down in gameplay */
+	public void AddScore(int score)
+	{
+		int i = 0;
+		while (i < numScores) 
+		{
+			if (scores[i] == null) {
+				scores[i] = score;
+				SaveScores ();
+				return;
+			}
+			else if (scores[i] >= score) {
+				i++;
+			}
+			else {
+				int temp = scores[i];
+				scores[i] = score;
+				score = temp;
+				i++;
+			}
+		}
+
+		SaveScores ();
+	}
+
+	/** Loads player scores from memory */
+	private void LoadScores()
+	{
+		for (int i=0; i < numScores; i++)
+		{
+			if (PlayerPrefs.HasKey("score" + i))
+				scores[i] = PlayerPrefs.GetInt("score" + i);
+		}
+	}
+
+	/** Saves current player scores to memory */
+	private void SaveScores()
+	{
+		for (int i=0; i < numScores; i++)
+		{
+			if (scores[i] != null)
+				PlayerPrefs.SetInt("score" + i, scores[i]);
+		}
+
+		PlayerPrefs.Save ();
+	}
+
+	/** Removes all of the player's high scores from memory */
+	public void ClearScores()
+	{
+		PlayerPrefs.DeleteAll ();
+		scores = new int[numScores];
+	}
+}
