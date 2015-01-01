81a824f67536854c9a54d85ae3facce182c8ce51
CogMonocle <jan89@drexel.edu>
2015-01-01 17:39:03 -0500

    Loading Screen

---
+++ Starbright Game/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingScreen : MonoBehaviour {
+
+	public string level;
+
+	// Use this for initialization
+	void Start () {
+		Application.LoadLevel (level);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
