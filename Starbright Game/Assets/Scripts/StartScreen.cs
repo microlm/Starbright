9e79cf34eb181d445a6a788091a850c233b7853b
ggetz <ggetz881@gmail.com>
2015-01-21 17:43:07 -0500

    Added logo screen, fixed menu aspect ratio

---
+++ Starbright Game/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour {
+
+	public float time;
+
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0f )
+			Game.Instance.GoToMenu ("Menu");
+	}
+}
