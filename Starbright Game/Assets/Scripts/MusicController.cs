0e7ca38c99a94a6ec91154fe4eabd91a257dbc04
ggetz <ggetz881@gmail.com>
2015-02-23 22:34:27 -0500

    Added Leveling Music
    Added MusicController.cs script. It has methods LevelUp() and
    LevelDown() that are called in ProgressCircle.cs. It takes an array of
    AudioSources in the inspector.

---
+++ Starbright Game/Assets/Scripts/MusicController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+
+	public AudioSource[] Tracks;
+
+	private int CurrentTrack;
+
+	public static MusicController Instance { get; private set; }
+
+	// Use this for initialization
+	void Start () {
+		CurrentTrack = 0;
+		Tracks [CurrentTrack].Play ();
+		Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LevelUp()
+	{
+		if (CurrentTrack < Tracks.Length - 1)
+		{
+			float time = Tracks [CurrentTrack].time;
+			Tracks [CurrentTrack].Stop ();
+			CurrentTrack++;
+			Tracks [CurrentTrack].time = time;
+			Tracks [CurrentTrack].Play ();
+		}
+
+	}
+
+	public void LevelDown()
+	{
+		if (CurrentTrack > 0)
+		{
+			float time = Tracks [CurrentTrack].time;
+			Tracks [CurrentTrack].Stop ();
+			CurrentTrack--;
+			Tracks [CurrentTrack].time = time;
+			Tracks [CurrentTrack].Play ();
+		}
+		
+	}
+}
